9e3105f031d1c846890513b221c2f765e9c44797
lipheris <lipheris@gmail.com>
2024-06-17 14:42:48 +0800

    新增PictureNotFoundException

--- src/main/java/com/ispan/aiml05/group6/project/exception/PictureNotFoundException.java
+++ src/main/java/com/ispan/aiml05/group6/project/exception/PictureNotFoundException.java
@@ -2,6 +2,10 @@ package com.ispan.aiml05.group6.project.exception;
 
 public class PictureNotFoundException extends RuntimeException {
 
+	public PictureNotFoundException(String message) {
+		super(message);
+	}
+
 	private static final long serialVersionUID = 1L;
 	
 }
