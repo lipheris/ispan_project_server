5e2bdbb6855d0a320a44f20b56e7b179a15c2f28
lipheris <lipheris@gmail.com>
2024-06-20 16:55:02 +0800

    修改DTO屬性接收base64字串

--- src/main/java/com/ispan/aiml05/group6/project/converter/PictureConverter.java
+++ src/main/java/com/ispan/aiml05/group6/project/converter/PictureConverter.java
@@ -1,31 +1,54 @@
 package com.ispan.aiml05.group6.project.converter;
 
 import java.lang.reflect.Field;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
 
 import com.ispan.aiml05.group6.project.dto.PictureDTO;
 import com.ispan.aiml05.group6.project.entity.Picture;
 import com.ispan.aiml05.group6.project.exception.PictureDTOException;
 
 public class PictureConverter {
+	private static final Decoder decoder = Base64.getDecoder();
+	private static final Encoder encoder = Base64.getEncoder();
 
 	public static Picture convert(PictureDTO pictureDto) throws PictureDTOException {
+		if (pictureDto == null) {
+			throw new PictureDTOException("PictureDTO is null");
+		}
+
+		byte[] image = null;
+
+		if (pictureDto.getBase64Image() != null) {
+			try {
+				image = decoder.decode(pictureDto.getBase64Image());
+			} catch (IllegalArgumentException e) {
+				throw new PictureDTOException("Invalid Base64 image data");
+			}
+
+			if (image.length > 16_000_000) { // 假設最大允許大小為16MB
+				throw new PictureDTOException("Image too large");
+			}
+		}
 		Picture convertedPicture = Picture.builder()
 				.id(pictureDto.getId())
 				.createdAt(pictureDto.getCreatedAt())
 				.location(pictureDto.getLocation())
+				.image(image)
 				.build();
 		double[][] points = pictureDto.getPoints();
 		try {
-			for (int i = 1; i <= Math.min(points.length, 17) ; i++) {
+			for (int i = 1; i <= Math.min(points.length, 17); i++) {
 				String xFieldName = "x" + i;
 				String yFieldName = "y" + i;
 				Field xField = Picture.class.getDeclaredField(xFieldName);
-			    Field yField = Picture.class.getDeclaredField(yFieldName);
+				Field yField = Picture.class.getDeclaredField(yFieldName);
 				// 取消私有屬性的封裝性
-				xField.setAccessible(true); 
-				yField.setAccessible(true); 
-				xField.set(convertedPicture, points[i-1][0]);
-				yField.set(convertedPicture, points[i-1][1]);
+				xField.setAccessible(true);
+				yField.setAccessible(true);
+				xField.set(convertedPicture, points[i - 1][0]);
+				yField.set(convertedPicture, points[i - 1][1]);
 			}
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
@@ -36,7 +59,16 @@ public class PictureConverter {
 	}
 
 	public static PictureDTO convert(Picture picture) {
+		if (picture == null) {
+			throw new PictureDTOException("Picture is null");
+		}
+		
+		String base64Image = null;
 		double[][] points = new double[17][2];
+
+		if(picture.getImage() != null)
+		base64Image = encoder.encodeToString(picture.getImage());
+
 		points[0][0] = picture.getX1();
 		points[0][1] = picture.getY1();
 		points[1][0] = picture.getX2();
@@ -71,10 +103,12 @@ public class PictureConverter {
 		points[15][1] = picture.getY16();
 		points[16][0] = picture.getX17();
 		points[16][1] = picture.getY17();
+
 		return PictureDTO.builder()
 				.id(picture.getId())
 				.createdAt(picture.getCreatedAt())
 				.location(picture.getLocation())
+				.base64Image(base64Image)
 				.points(points)
 				.build();
 	}
--- src/main/java/com/ispan/aiml05/group6/project/dto/PictureDTO.java
+++ src/main/java/com/ispan/aiml05/group6/project/dto/PictureDTO.java
@@ -2,6 +2,8 @@ package com.ispan.aiml05.group6.project.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,8 @@ public class PictureDTO {
 	private long id;
 	private LocalDateTime createdAt;
 	private String location;
+	@JsonProperty("image")
+	private String base64Image;
 	@Builder.Default
 	private double[][] points = new double[17][2];
 }
